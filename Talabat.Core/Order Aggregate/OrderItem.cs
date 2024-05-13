6de0fbe7076cfb49d669307dbf08c5ec2e25c890
asus <asus@AHMED-EMAM>
2024-05-13 18:23:56 +0200

    Part 01 Order Module - Entities

---
+++ Talabat.Core/Order Aggregate/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Order_Aggregate
+{
+	public class OrderItem : BaseEntity
+	{
+        public ProductItemOrdered Product { get; set; } = null!;
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
