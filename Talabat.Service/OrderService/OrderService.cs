b2775988ba8ae41096f3e1f11fbedcb5579ee844
asus <asus@AHMED-EMAM>
2024-05-13 20:26:44 +0200

    Part 05 Order Service

---
+++ Talabat.Service/OrderService/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Order_Aggregate;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Service.OrderService
+{
+	public class OrderService : IOrderService
+	{
+		public Task<Order> CreateOrderAsync(string BuyerEmail, string basketId, string deliveryMethod, Address shippingAddress)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Order> GetOrderByIdForUserAsync(string buyerEmail, int orderId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
